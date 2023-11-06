55b49c5dba8e721101eb59c900897714d413ce5d
vlados2108 <verlov.vladislav@mail.ru>
2023-11-06 14:56:40 +0300

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+import { ReactElement, ReactNode } from 'react'
+import './layout.scss'
+
+const RootLayout = ({ children }: { children: ReactNode }): ReactElement => {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    )
 }
+export default RootLayout
